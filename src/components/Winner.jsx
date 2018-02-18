52d78ce20edf4299c690961974e3b5822465bd3c
Mateus Costa <mateuscomp@gmail.com>
2018-02-18 16:31:36 +0100

    split into smaller components

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import Winner from './Winner';
-import Vote from './Vote';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
-    return <div>
-      {this.props.winner ?
-        <Winner ref="winner" winner={this.props.winner} /> :
-        <Vote {...this.props} />}
+    return <div className="winner"> Winner is {this.props.winner}!
     </div>;
   }
 });
